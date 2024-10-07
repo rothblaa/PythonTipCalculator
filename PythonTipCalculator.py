6456baab47d096cd8d92eec9415503c3f47f765f
rothblaa <adam.j.rothblatt@outlook.com>
2024-10-07 13:17:49 -0500

    Initial code commit

--- PythonTipCalculator.py
+++ PythonTipCalculator.py
@@ -1,1 +1,25 @@
-print("Hello" [0])
+
+#tasks
+#-------------------------------
+
+#make a tip calculator
+#display a welcome message
+print("Welcome to the tip calculator!")
+
+#input for total bill
+bill = float(input("What was the total bill?\n $"))
+
+#input for how much tip you'd like to give: 10, 12, or 15
+tip = int(input("How much tip would you like to leave?\n"))
+monetizedTip = float(1 + (tip / 100))
+
+#input for how many people will split the bill
+people = int(input("How many people are going to split the bill?\n"))
+
+#logic to figure out the total
+totalBillPerPerson = (bill / people) * monetizedTip
+
+#give the total number each person should pay rounded to 2 decimal places 
+print("The total each person needs to pay is: $" + str(totalBillPerPerson))
+
+
